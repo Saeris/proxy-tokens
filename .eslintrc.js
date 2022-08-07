39784d26d1cf17f0702063c981059eaad253f065
Drake Costa <drake@saeris.io>
2022-08-07 00:29:47 -0700

    fix: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 // @ts-check
 
-// @ts-ignore
-require(`@saeris/eslint-config/patch`);
-
 /**
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  extends: [require.resolve(`@saeris/eslint-config`)],
+  extends: ["@saeris/eslint-config"],
   ignorePatterns: [`*.js`]
 };
- 
